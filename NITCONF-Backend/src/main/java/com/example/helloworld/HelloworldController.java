30d6427c4fe909d173950ba7cbaf29f052d63e8d
unknown <hsai19172@gmail.com>
2024-01-22 19:47:21 +0530

    Basic Setup of the Frontend and the Backend

---
+++ NITCONF-Backend/src/main/java/com/example/helloworld/HelloworldController.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloworldController {
+
+	@GetMapping("/")
+	public String getString() {
+		return "Hello World!";
+	}
+
+	@GetMapping("/name")
+	public String getName() {
+		return new String("Hello World! My name is Deekshu.");
+	}
+	
+
+}
